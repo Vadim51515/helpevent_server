b81abd1aced62523a8409c1414c8cd115c516b62
Vadim51515 <68582499+Vadim51515@users.noreply.github.com>
2021-04-10 22:29:47 +0500

    server full

--- server/index.js
+++ server/index.js
@@ -61,6 +61,81 @@ app.get('/events',(req,res) => {
     })
    
 })
+app.get('/resources',(req,res) => {
+    const sqlSelect = "SELECT * FROM `resources` WHERE 1"
+    db.query(sqlSelect, (err,result) => {
+       res.send(result)
+       return result
+    })
+})
+app.patch('/patch/resources',(req,res) => {
+    const name = req.body.name
+    const quantity = req.body.quantity
+    const price = req.body.price
+    const id = req.body.id
+    console.log(req.body.number);
+     const sqlInsert = " UPDATE `resources` SET `name`=(?),`quantity`=(?),`price`=(?) WHERE `id` =(?);"
+        db.query(sqlInsert,[name, quantity, price,id], (err,result) => {
+        console.log(err);
+        res.send(result)
+        console.log(result);
+     })
+})
+app.post('/newresources',(req,res) => {
+    const name = req.body.name
+    const quantity = req.body.quantity
+    const price = req.body.price
+    const idEvent = req.body.idEvent
+    console.log(req.body.number);
+     const sqlInsert = "INSERT INTO resources (`idEvent`, `name`, `quantity`, `price`) VALUES (?, ?, ?, ?);"
+        db.query(sqlInsert,[idEvent, name,quantity,price], (err,result) => {
+        console.log(err);
+        res.send(result)
+        console.log(result);
+     })
+})
+app.delete('/delresources/:id',(req,res) => {
+    const id = req.params.id
+    console.log(req.body.id);
+     const sqlInsert = "DELETE FROM `resources` WHERE `id`= (?);"
+        db.query(sqlInsert,id, (err,result) => {
+        console.log(err);
+        res.send(result)
+        console.log(result);
+     })
+})
+app.post('/addnewevent',(req,res) => {
+    const name = req.body.name
+    const idUser = req.body.idUser
+    console.log(req.body.number);
+     const sqlInsert = "INSERT INTO event (`name`, `idUser`) VALUES (?, ?);"
+        db.query(sqlInsert,[name,idUser], (err,result) => {
+        console.log(err);
+        res.send(result)
+        console.log(result);
+     })
+})
+app.patch('/patch/event',(req,res) => {
+    const name = req.body.name
+    const id = req.body.id
+    console.log(req.body.number);
+     const sqlInsert = " UPDATE `event` SET `name`=(?) WHERE `id` =(?);"
+        db.query(sqlInsert,[name, id], (err,result) => {
+        console.log(err);
+        res.send(result)
+        console.log(result);
+     })
+})
+app.delete('/delevent/:id',(req,res) => {
+    const id = req.params.id
+    console.log(req.body.id);
+     const sqlInsert = "DELETE FROM `event` WHERE `id`= (?);"
+        db.query(sqlInsert,id, (err,result) => {
+        console.log(err);
+        res.send(result)
+        console.log(result);
+     })
+})
 app.listen('3001',() => {
     console.log("start server 3001 port");
 })
